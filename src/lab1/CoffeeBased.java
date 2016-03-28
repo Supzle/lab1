ce44a9011ef0944ba1cc28170c91d5ee85111f86
zhangchao <13307130288@fudan.edu.cn>
2016-03-28 19:34:43 +0800

    1.new coffee Decaf 2.allow choose Decaf Mocha 3.new size Grant

---
+++ src/lab1/CoffeeBased.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class CoffeeBased implements SizeFactor {
+
+	@Override
+	public double sizeCost(String size) {
+		if (size.toLowerCase().equals("small")) {
+			return 0.4;
+		} else if (size.toLowerCase().equals("medium")) {
+			return 0.7;
+		} else if (size.toLowerCase().equals("large")) {
+			return 1.0;
+		} else {
+			return 1.3;
+		}
+	}
+
+}
---
+++ src/lab1/TeaBased.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class TeaBased implements SizeFactor {
+
+	@Override
+	public double sizeCost(String size) {
+		if (size.toLowerCase().equals("small")) {
+			return 0.2;
+		} else if (size.toLowerCase().equals("medium")) {
+			return 0.5;
+		} else if (size.toLowerCase().equals("large")) {
+			return 0.7;
+		} else {
+			return 0.9;
+		}
+	}
+
+}
